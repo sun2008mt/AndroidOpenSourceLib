906ad9fa968ded728e64ae14c48d0c2550b9b736
Marc <710641245@qq.com>
2018-04-02 18:13:22 +0800

    add bottom-navigation-bar

--- app/src/main/java/whu/sun/androidopensourcelibrarytest/MainActivity.java
+++ app/src/main/java/whu/sun/androidopensourcelibrarytest/MainActivity.java
@@ -2,8 +2,10 @@ package whu.sun.androidopensourcelibrarytest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.ashokvarma.bottomnavigation.BottomNavigationBar;
+import com.ashokvarma.bottomnavigation.BottomNavigationItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +19,27 @@ public class MainActivity extends AppCompatActivity {
 
     private void initView() {
         BottomNavigationBar bottomNavigationBar = findViewById(R.id.bottom_navigation_bar);
+        bottomNavigationBar.addItem(new BottomNavigationItem(R.drawable.account, "Test1"))
+                .addItem(new BottomNavigationItem(R.drawable.account, "Test2"))
+                .addItem(new BottomNavigationItem(R.drawable.account, "Test3"))
+                .addItem(new BottomNavigationItem(R.drawable.account, "Test4"))
+                .addItem(new BottomNavigationItem(R.drawable.account, "Test5"))
+                .initialise();
+        bottomNavigationBar.setTabSelectedListener(new BottomNavigationBar.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(int position) {
+                Toast.makeText(MainActivity.this, "Tab" + position + "selected...", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onTabUnselected(int position) {
+                Toast.makeText(MainActivity.this, "Tab" + position + "unselected...", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onTabReselected(int position) {
+                Toast.makeText(MainActivity.this, "Tab..." + position + "reselected...", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
