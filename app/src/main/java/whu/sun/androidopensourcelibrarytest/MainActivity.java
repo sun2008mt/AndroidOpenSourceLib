b3240108c79239cb452f1d9e3b525d5d04f98965
Marc <710641245@qq.com>
2018-04-02 18:32:40 +0800

    add pull to refresh

--- app/src/main/java/whu/sun/androidopensourcelibrarytest/MainActivity.java
+++ app/src/main/java/whu/sun/androidopensourcelibrarytest/MainActivity.java
@@ -2,12 +2,20 @@ package whu.sun.androidopensourcelibrarytest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import com.ashokvarma.bottomnavigation.BottomNavigationBar;
 import com.ashokvarma.bottomnavigation.BottomNavigationItem;
 
+import in.srain.cube.views.ptr.PtrDefaultHandler;
+import in.srain.cube.views.ptr.PtrFrameLayout;
+import in.srain.cube.views.ptr.header.MaterialHeader;
+import in.srain.cube.views.ptr.util.PtrLocalDisplay;
+
 public class MainActivity extends AppCompatActivity {
+
+    private PtrFrameLayout ptrFrameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +26,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
+        ptrFrameLayout = findViewById(R.id.ptr_frame);
+        //MaterialDesign头部风格
+        MaterialHeader header = new MaterialHeader(this);
+        header.setPadding(0, PtrLocalDisplay.dp2px(15), 0, 0);
+        ptrFrameLayout.setHeaderView(header);
+        ptrFrameLayout.addPtrUIHandler(header);
+        ptrFrameLayout.setPtrHandler(new PtrDefaultHandler() {
+            @Override
+            public void onRefreshBegin(final PtrFrameLayout frame) {
+                frame.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        frame.refreshComplete();
+                    }
+                }, 1800);
+            }
+
+            @Override
+            public boolean checkCanDoRefresh(PtrFrameLayout frame, View content, View header) {
+                return super.checkCanDoRefresh(frame, content, header);
+            }
+        });
+
         BottomNavigationBar bottomNavigationBar = findViewById(R.id.bottom_navigation_bar);
         bottomNavigationBar.addItem(new BottomNavigationItem(R.drawable.account, "Test1"))
                 .addItem(new BottomNavigationItem(R.drawable.account, "Test2"))
